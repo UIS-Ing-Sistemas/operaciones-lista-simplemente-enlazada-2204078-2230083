c3bf987973cc02781873e42f42c77131982aa665
JuGaGar <159270042+JuGaGar@users.noreply.github.com>
2024-02-28 09:46:12 -0500

    Update Base_código.py

--- Base_código.py
+++ Base_código.py
@@ -5,6 +5,12 @@
 #Buscar un elemento por su valor (devuelve Verdadero o Falso)
 #Contar elementos de la lista
 #Indicar si la lista está vacía
+from operator import truediv
+from pickle import TRUE
+from tkinter import FALSE
+from xmlrpc.client import boolean
+
+
 class Nodo:
     def __init__(self, valor):
         self.data = valor
@@ -24,10 +30,40 @@ class ListaSE:
             nNodo.siguiente = self.cabeza
             self.cabeza = nNodo
 
-
-Lista1=ListaSE()
-Lista1.AgregarInicio(5)
-print (Lista1.cabeza.data)
-Lista1.AgregarInicio(8)
-print (Lista1.cabeza.data)
-print (Lista1.cabeza.siguiente.data)
+    def AgregarFinal(self,valor):
+        nNodo = valor
+        nActual = self.cabeza
+        if nActual.siguiente is None:
+            nActual.siguiente = nNodo
+        else:
+            nActual = nActual.siguiente
+            
+    def EliminarInicio(self):
+        if self.cabeza is not None:
+            sigTemp = self.cabeza.siguiente
+            self.cabeza. #borrar elemento actual(comando por agregar)
+            self.cabeza=sigTemp
+            
+    def EliminarFinal(self):
+        nActual = self.cabeza
+        buscando = TRUE
+        while buscando:
+            if nActual.siguiente is None:
+                buscando = FALSE
+                nActual. #borrar elemento actual(comando por agregar)
+            else:
+                nActual = nActual.siguiente
+            
+    def BuscarValor(self,vab):
+        nActual = self.cabeza #valor observado
+        buscando = TRUE
+        while buscando:
+            if nActual.valor == vab: #si el nodo observado tiene valor igual al vab
+                buscando = FALSE
+                return 1 # devolver 1 (verdadero)
+            else:
+                if nActual.siguiente is None: #si el valor no es igual, se revisa si el nodo apunt a un nodo distinto
+                    buscando = FALSE
+                    return 0 # si no tiene nodo siguiente, se devuelve 0 (falso)
+                else:
+                    nActual = nActual.siguiente # si sí tiene nodo siguiente, se observa el próximo nodo
